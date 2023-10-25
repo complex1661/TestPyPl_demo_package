359f0ddab7e8cb11d1ab67eea77840f9df85c699
complex1661 <144210657+complex1661@users.noreply.github.com>
2023-10-25 16:08:41 +0800

    Add files via upload

---
+++ test_package/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="python_test",
+    version="0.0.1",
+    author="Complex",
+    author_email="examoke@gmail.com",
+    description="A small example package.",
+    long_description="A longer example package description.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
